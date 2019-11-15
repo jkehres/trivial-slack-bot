d5be3fda9f447be44d5f12c5b7bb1b76daa1f0ee
Maya Jones <Maya.Jones@D2L.com>
2019-11-15 12:05:58 -0500

    Using libraries!

--- src/stream.js
+++ src/stream.js
@@ -1,99 +1,27 @@
 'use strict';
 
 const logger = require('./logger');
+const groupBy = require('lodash.groupby');
+const AWS = require('aws');
+const allowedEvents = ['MODIFY', 'INSERT'];
 
 module.exports.handler = async (event) => {
-	event = decodeEvent({event});
-	logger.debug({event}, 'Event received');
-};
+	const unmarshalledRecords = event.Records
+		.filter(record => record.eventName && allowedEvents.includes(record.eventName))
+		.map(record => AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage));
 
-class Question {
-	static get dbKeys() {
-		return ['date', 'channel'];
-	}
-
-	get answers() {
-		return this._answers;
-	}
-
-	get channel() {
-		return this._channel;
-	}
-
-	get date() {
-		return this._date;
-	}
-
-	constructor(image) {
-		this._date = image.date.S;
-		this._channel = image.channel.S;
-		this._answers = [];
-		Object.keys(image).forEach(key => {
-			if (!Question.dbKeys.includes(key) && image[key] && image[key].M) {
-				this._answers.push({
-					name: key,
-					answer: image[key].M.answer && image[key].M.answer.BOOL,
-					timestamp: image[key].M.timestamp && image[key].M.timestamp.N,
-				});
-			}
-		});
-		this._answers.sort(this._compareAnswers);
-	}
-
-	_compareAnswers(a, b) {
-		if (a.timestamp < b.timestamp) {
-			return 1
-		} else if (a.timestamp > b.timestamp) {
-			return -1
+	const groupedRecords = groupBy(unmarshalledRecords, record => {
+		if (!record.channel || !record.date) {
+			return 'deleteMe';
 		}
 
-		if (a.name < b.name ) {
-			return 1
-		} else if (a.name > b.name) {
-			return -1;
-		}
-
-		return 0;
-	}
-}
-
-class DynamoDbRecord {
-	constructor(record) {
-		if(!record || !record.dynamodb || !record.dynamodb.Keys || !record.dynamodb.SequenceNumber) {
-			throw new Error('Bad Record');
-		}
-		this._keys = record.dynamodb.Keys;
-		this._sequenceNumber = record.dynamodb.SequenceNumber;
-		this._newImage = record.dynamodb.NewImage;
-	}
-
-	decodeImage(imageType) {
-		return new imageType(this._newImage);
-	}
-
-	uniqueId() {
-		var b = new Buffer(JSON.stringify(this._keys));
-		return b.toString('base64');
-	}
-
-	isMoreNewThan(record) {
-		return record._sequenceNumber < this._sequenceNumber;
-	}
-}
-
-function decodeEvent({event}) {
-	const records = {};
-	event.Records.forEach(record => {
-		try {
-			record = new DynamoDbRecord(record);
-			const uniqueRecordId = record.uniqueId();
-			if (!records[uniqueRecordId] || record.isMoreNewThan(records[uniqueRecordId])) {
-				records[uniqueRecordId] = record;
-			}
-		} catch(error) {
-			logger.debug(error, 'Event received');
-		}
+		return `${record.channel}.${record.date}`;
 	});
+	delete(groupedRecords['deleteMe']); // delete bad events
 
-	return Object.values(records).map(element => element.decodeImage(Question));
-}
+	for (const records of Object.entries(groupedRecords)) {
+		const latestRecord = records.pop();
+		logger.debug({latestRecord}, 'Event received');
+	}
+
+};
